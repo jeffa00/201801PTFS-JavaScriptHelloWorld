ba34983130777a306a64881b7176865ee7b7a11a
jeffa <jeffa00@gmail.com>
2018-03-19 20:46:14 -0400

    Wrote hello world in app.js

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-console.log("Hello, World!");
+// One equal sign is called the assignment operator.
+// This is because we are assigning a value to a variable.
+var whoToGreet = "Duluth";
+var loopNumber = 5;
+
+// Principle of software development:
+// The DRY Principle.
+
+// Don't Repeat Yourself
+
+for(var i=0; i < loopNumber; i = i + 1){
+    console.log("Index: " + i + " %2 " + i % 2);
+    if (i % 2 != 0){
+        console.log(i + ") Hello, " + whoToGreet +  "!");
+    }
+}
+
+console.log("Done");
